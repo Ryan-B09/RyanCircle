11d4638f0bf5ac30bfa731772c4145003f109985
VinceCube <131001033+Ryan-B09@users.noreply.github.com>
2023-04-24 15:13:16 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,3 +24,30 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+const CACHE_NAME = 'guess-number-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/style.css',
+  '/script.js',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/data.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
